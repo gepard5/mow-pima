8cbc9f028ad45c4090ce15eace3458bafd26a124
Michał Glinka <michal.glinka95@gmail.com>
2019-06-04 00:37:23 +0200

    Test

--- mow.r
+++ mow.r
@@ -14,22 +14,23 @@ repeat{
 }
 test_data<-pd.test
 train_data<-pd.train
-g_columns <- ncol(PimaData) - 0.01
-g_maxIter <- 100
-g_popSize <- 50
-g_runNumber <- 100
-g_elitism <- 0.05
+g_columns <- ncol(PimaData) - 1
+g_maxIter <- 20
+g_popSize <- 20
+g_runNumber <- 20
+g_elitism <- 0.2
 g_crossoverChance <- 0.8
-g_mutationChance <- 0.1
-g_tree_levels <- 5
+g_mutationChance <- 0.2
+g_tree_levels <- 4
 
 library(GA)
 source("./functions.r")
 
 
-#print("Test attribute tree")
-#ga1 <- test_attribute_tree(g_maxIter, g_popSize, g_runNumber, g_elitism, g_crossoverChance, g_mutationChance, g_tree_levels)
-#summary(ga1)
+print("Test attribute tree")
+ga1 <- test_attribute_tree(g_maxIter, g_popSize, g_runNumber, g_elitism, g_crossoverChance, g_mutationChance, g_tree_levels)
+print(ga1)
+print(summary(ga1))
 #print("Test attribute threshold tree")
 #ga2 <- test_attr_thresh_tree(g_maxIter, g_popSize, g_runNumber, g_elitism, g_crossoverChance, g_mutationChance, g_tree_levels)
 #summary(ga2)
